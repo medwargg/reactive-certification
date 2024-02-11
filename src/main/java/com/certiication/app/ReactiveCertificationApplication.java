9f58e8911a9dce528c4a4da14d471c3ddf3bf857
Cesar Guerrero <medwargg@gmail.com>
2024-02-11 10:33:38 -0600

    Initial structure
    Add: Basic dependencies to use Spring Webflux, Reactive MongoDB and Spring Security.

---
+++ src/main/java/com/certiication/app/ReactiveCertificationApplication.java
@@ -0,0 +1,13 @@
+package com.certiication.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCertificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveCertificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/certiication/app/ReactiveCertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certiication.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCertificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
